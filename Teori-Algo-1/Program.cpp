04ab7a2913516990c453b44c3b9b54347887b091
Indy aurellia <indyaurellia@gmail.com>
2024-06-25 01:21:07 +0700

    Teori-1

---
+++ Teori-Algo-1/Program.cpp
@@ -0,0 +1,27 @@
+#### Indy Aurellia Az Zahra
+#### 2311110020 [S1SD-04-A]
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool cekDuplikat(int arr[]) {
+  int panjangArr = 4;
+
+  sort(arr, arr + panjangArr);
+
+  for (int i = 0; i < panjangArr - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+int main(void) {
+  int arrA[] = {2, 1, 3, 1};
+  cout << cekDuplikat(arrA);
+  return 0;
+}
